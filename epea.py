5352d6aed9209a3eb32df50adbd723ce521e11e2
AdamBignell <adambignell@gmail.com>
2020-03-09 21:26:30 -0700

    Initialization: Created bare bones skeleton code for ICTS and EPEA Solvers

---
+++ epea.py
@@ -0,0 +1,41 @@
+import time as timer
+import heapq
+import random
+from single_agent_planner import compute_heuristics, a_star, get_location, get_sum_of_cost
+
+class EPEASolver(object):
+    """A high-level EPEA* search."""
+
+    def __init__(self, my_map, starts, goals):
+        """my_map   - list of lists specifying obstacle positions
+        starts      - [(x1, y1), (x2, y2), ...] list of start locations
+        goals       - [(x1, y1), (x2, y2), ...] list of goal locations
+        """
+
+        self.my_map = my_map
+        self.starts = starts
+        self.goals = goals
+        self.num_of_agents = len(goals)
+
+        self.num_of_generated = 0
+        self.num_of_expanded = 0
+        self.CPU_time = 0
+
+        self.open_list = []
+
+        # compute heuristics for the low-level search
+        self.heuristics = []
+        for goal in self.goals:
+            self.heuristics.append(compute_heuristics(my_map, goal))
+
+    def find_solution(self):
+        """ Finds paths for all agents from their start locations to their goal locations
+        """
+        print("\nFinding EPEA* Solution...")
+        ######## Fill in the EPEA* Algorithm here #########
+
+
+
+        ###################################################
+
+        return None
---
+++ icts.py
@@ -0,0 +1,41 @@
+import time as timer
+import heapq
+import random
+from single_agent_planner import compute_heuristics, a_star, get_location, get_sum_of_cost
+
+class ICTSSolver(object):
+    """A high-level ICTS search."""
+
+    def __init__(self, my_map, starts, goals):
+        """my_map   - list of lists specifying obstacle positions
+        starts      - [(x1, y1), (x2, y2), ...] list of start locations
+        goals       - [(x1, y1), (x2, y2), ...] list of goal locations
+        """
+
+        self.my_map = my_map
+        self.starts = starts
+        self.goals = goals
+        self.num_of_agents = len(goals)
+
+        self.num_of_generated = 0
+        self.num_of_expanded = 0
+        self.CPU_time = 0
+
+        self.open_list = []
+
+        # compute heuristics for the low-level search
+        self.heuristics = []
+        for goal in self.goals:
+            self.heuristics.append(compute_heuristics(my_map, goal))
+
+    def find_solution(self):
+        """ Finds paths for all agents from their start locations to their goal locations
+        """
+        print("\nFinding ICTS Solution...")
+        ######### Fill in the ICTS Algorithm here #########
+
+
+
+        ###################################################
+
+        return None
