a7326441e24da87e1e4e276e573b40f1003cf6ff
AdamBignell <adambignell@gmail.com>
2020-03-14 18:20:36 -0700

    Map Utils: Refactor map loading out of run_experiments

--- run_experiments.py
+++ run_experiments.py
@@ -1,72 +1,13 @@
 #!/usr/bin/python
 import argparse
 import glob
-from pathlib import Path
 from icts import ICTSSolver
 from epea import EPEASolver
 from visualize import Animation
 from single_agent_planner import get_sum_of_cost
+import map_utils as util
 
 SOLVER = "ICTS"
-
-def print_mapf_instance(my_map, starts, goals):
-    print('Start locations')
-    print_locations(my_map, starts)
-    print('Goal locations')
-    print_locations(my_map, goals)
-
-
-def print_locations(my_map, locations):
-    starts_map = [[-1 for _ in range(len(my_map[0]))] for _ in range(len(my_map))]
-    for i in range(len(locations)):
-        starts_map[locations[i][0]][locations[i][1]] = i
-    to_print = ''
-    for x in range(len(my_map)):
-        for y in range(len(my_map[0])):
-            if starts_map[x][y] >= 0:
-                to_print += str(starts_map[x][y]) + ' '
-            elif my_map[x][y]:
-                to_print += '@ '
-            else:
-                to_print += '. '
-        to_print += '\n'
-    print(to_print)
-
-
-def import_mapf_instance(filename):
-    f = Path(filename)
-    if not f.is_file():
-        raise BaseException(filename + " does not exist.")
-    f = open(filename, 'r')
-    # first line: #rows #columns
-    line = f.readline()
-    rows, columns = [int(x) for x in line.split(' ')]
-    rows = int(rows)
-    columns = int(columns)
-    # #rows lines with the map
-    my_map = []
-    for r in range(rows):
-        line = f.readline()
-        my_map.append([])
-        for cell in line:
-            if cell == '@':
-                my_map[-1].append(True)
-            elif cell == '.':
-                my_map[-1].append(False)
-    # #agents
-    line = f.readline()
-    num_agents = int(line)
-    # #agents lines with the start/goal positions
-    starts = []
-    goals = []
-    for a in range(num_agents):
-        line = f.readline()
-        sx, sy, gx, gy = [int(x) for x in line.split(' ')]
-        starts.append((sx, sy))
-        goals.append((gx, gy))
-    f.close()
-    return my_map, starts, goals
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Compares ICTS and EPEA* Algorithms')
@@ -82,8 +23,8 @@ if __name__ == '__main__':
     for file in sorted(glob.glob(args.instance)):
 
         print("***Import an instance***")
-        my_map, starts, goals = import_mapf_instance(file)
-        print_mapf_instance(my_map, starts, goals)
+        my_map, starts, goals = util.import_mapf_instance(file)
+        util.print_mapf_instance(my_map, starts, goals)
         
         paths = []
         if args.solver == "ICTS":
