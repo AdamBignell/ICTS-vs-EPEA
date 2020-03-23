5acc2472927fab6de391582d42b0898aff69ba5f
markom <markom@novuscom.net>
2020-03-22 18:38:14 -0700

    started epea* algorithm

--- epea.py
+++ epea.py
@@ -34,8 +34,44 @@ class EPEASolver(object):
         print("\nFinding EPEA* Solution...")
         ######## Fill in the EPEA* Algorithm here #########
 
+        start_node = starts
+        compute_heuristics(start_node)
+        self.open_list.append(start_node)
+        while(len(self.open_list) != 0):
+            current_node = get_lowest_stored_cost(open_list)
+            if current_node == goal_state:
+                return "success"
+            #set something to value of OSF? not sure what goes here
+            #(N, F_next(n)) = OSF(n)
+            for child in generate_children(current_node):
+                compute_heuristics(child)
+                compute_cost(child)
+                #not sure what the next few lines mean
+                #if f(n_c) != F(n):
+                    #if f(n_c) > F(n):
+                        #F_next(n) = min(F_next(n), f(n_c))
+                        #discard n_c
+                        #continue
+                #check for duplicates
+            
+            #if F_next(n) = inf:
+                #put n into closed
+            #else:
+                #F(n) = F_next(n)
+                self.open_list.append(curr)
 
-
+            return None
         ###################################################
+    
+    def collapse(self, node):
+        pass
 
-        return None
+    def get_lowest_stored_cost(self, open_list):
+        pass
+    
+    def generate_children(self, node):
+        pass
+    
+    def compute_cost(self, node):
+        pass
+
