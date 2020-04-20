5f28094690871e37098cb427359d6bdd77dfd0f7
AdamBignell <adambignell@gmail.com>
2020-04-19 19:55:03 -0700

    Graphing: Add graphs for node expansions

--- graph.py
+++ graph.py
@@ -17,15 +17,20 @@ plt.rcParams['figure.titlesize'] = 12
 colors = sbn.color_palette("husl")
 
 def save_graphs():
-
     # Generate Maze Graphs
     epea_batch_names = get_file_names('EPEA', 'maze')
     icts_batch_names = get_file_names('ICTS', 'maze')
     epea_results = get_all_results(epea_batch_names)
     icts_results = get_all_results(icts_batch_names)
 
-    maze_labels = get_maze_labels('EPEA')
-    save_runtime_graph(epea_results, icts_results, maze_labels)
+    maze_runtime_labels = get_runtime_labels('Maze')
+    save_runtime_graph(epea_results, icts_results, maze_runtime_labels, 'maze_runtime_medians.pdf')
+
+    maze_expansion_epea_labels = get_node_expansion_labels('EPEA*', 'Maze')
+    save_expansion_graph(epea_results, maze_expansion_epea_labels, 'maze_epea_expansions.pdf')
+
+    maze_expansion_icts_labels = get_node_expansion_labels('ICTS', 'Maze')
+    save_expansion_graph(icts_results, maze_expansion_icts_labels, 'maze_icts_expansions.pdf')
 
 def get_file_names(algorithm, prefix):
     batch_prefix_names = [prefix + '12x12', prefix + '25x25', prefix + '50x50', prefix + '100x100']
@@ -58,22 +63,26 @@ def get_results(filename):
                 results.append(new_result)
     return results
 
-def save_runtime_graph(epea_results, icts_results, labels):
-    epea_median_times = get_median_times(epea_results)
-    icts_median_times = get_median_times(icts_results)
-    save_beautiful_graph_medians(epea_median_times, icts_median_times, labels)
+def save_runtime_graph(epea_results, icts_results, labels, output_file):
+    epea_median_times = get_medians(epea_results, 'time')
+    icts_median_times = get_medians(icts_results, 'time')
+    save_beautiful_graph_medians(epea_median_times, icts_median_times, labels, output_file)
 
     # Lol don't use the below, the stddevs are HUGE
     #epea_means, epea_stddevs = get_means_and_stddevs(epea_results)
     #icts_means, icts_stddevs = get_means_and_stddevs(icts_results)
     #save_beautiful_graph_means(epea_means, epea_stddevs, icts_means, icts_stddevs, labels)
 
+def save_expansion_graph(results, labels, output_file):
+    median_expansions = get_medians(results, 'expansions')
+    save_beautiful_graph_medians(median_expansions, [], labels, output_file)
 
-def get_median_times(results):
-    result_times = results_to_times(results)
+
+def get_medians(results, stat):
+    result_nums = results_to_nums(results, stat)
     medians = []
-    for times in result_times:
-        medians.append(statistics.median(times))
+    for nums in result_nums:
+        medians.append(statistics.median(nums))
     return medians
 
 def get_means_and_stddevs(results):
@@ -85,21 +94,36 @@ def get_means_and_stddevs(results):
         stddevs.append(statistics.stdev(times))
     return means, stddevs
 
-def get_maze_labels(algorithm):
+def get_runtime_labels(instance_type):
     labels = {}
-    labels['x_title'] = 'Maze Size'
+    labels['x_title'] = instance_type + ' Instance Size'
     labels['x_labels'] = ['12x12', '25x25', '50x50', '100x100']
     labels['y_title'] = 'Runtime in Seconds'
-    labels['title'] = 'Median ' + algorithm + ' runtimes for increasingly large\nMaze Instances, using 3 agents'
+    labels['title'] = 'Comparison of Median ICTS and EPEA* runtimes for increasingly large\n' + instance_type + ' Instances, using 3 agents'
+    labels['colors'] = colors
     labels['legend'] = ['EPEA*', 'ICTS']
     return labels
 
-def save_beautiful_graph_medians(epea_medians, icts_medians, labels):
+def get_node_expansion_labels(algorithm, instance_type):
+    labels = {}
+    labels['x_title'] = instance_type + ' Instance Size'
+    labels['x_labels'] = ['12x12', '25x25', '50x50', '100x100']
+    if algorithm == 'ICTS':
+        labels['y_title'] = 'Number of High Level Node Expansions'
+        labels['colors'] = [colors[4]]
+    elif algorithm == 'EPEA*':
+        labels['y_title'] = 'Number of Node Expansions'
+        labels['colors'] = [colors[0]]
+    labels['title'] = 'Median ' + algorithm + ' Node Expansions for increasingly large\n' + instance_type + ' Instances, using 3 agents'
+    labels['legend'] = [algorithm]
+    return labels
+
+def save_beautiful_graph_medians(first_medians, second_medians, labels, title):
     fig, ax = plt.subplots(1, 1)
-    plt.plot(epea_medians)
-    if icts_medians != []:
-        plt.plot(icts_medians)
-    ticks = list(range(len(epea_medians)))
+    plt.plot(first_medians, c=labels['colors'][0])
+    if second_medians != []:
+        plt.plot(second_medians, c=labels['colors'][4])
+    ticks = list(range(len(first_medians)))
     ax.set_xticks(ticks)
     ax.set_xticklabels(labels['x_labels'], minor=False)
 
@@ -107,13 +131,12 @@ def save_beautiful_graph_medians(epea_medians, icts_medians, labels):
     plt.ylabel(labels['y_title'])
     plt.title(labels['title'])
     ax.legend(labels['legend'], facecolor='white')
-    
-    plt.show()
+    plt.savefig(title, bbox_inches='tight')
 
 def save_beautiful_graph_means(epea_means, epea_stddevs, icts_means, icts_stddevs, labels):
     fig, ax = plt.subplots(1, 1)
     plt.plot(epea_means, c=colors[0])
-    plt.plot(icts_means, c=colors[1])
+    plt.plot(icts_means, c=colors[4])
     ticks = list(range(len(epea_means)))
     ax.set_xticks(ticks)
     ax.set_xticklabels(labels['x_labels'], minor=False)
@@ -132,7 +155,7 @@ def save_beautiful_graph_means(epea_means, epea_stddevs, icts_means, icts_stddev
     
     plt.show()
 
-def results_to_times(results):
+def results_to_nums(results, stat):
     sizes = ['12x12', '25x25', '50x50', '100x100']
     results_by_size = []
     algorithm = results[0][0]['algorithm']
@@ -141,11 +164,17 @@ def results_to_times(results):
         setup_time = 'osf time'
     for i, size in enumerate(sizes):
         these_results = results[i]
-        times = []
+        nums = []
         for result in these_results:
-            this_time = result['time'] + result[setup_time]
-            times.append(this_time)
-        results_by_size.append(times)
+            if stat == 'time':
+                this_time = result['time'] + result[setup_time]
+                nums.append(this_time)
+            elif stat == 'expansions':
+                if "expanded nodes" not in result:
+                    nums.append(0)
+                else:
+                    nums.append(result['expanded nodes'])
+        results_by_size.append(nums)
     return results_by_size
 
 
