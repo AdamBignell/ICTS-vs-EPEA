2549af4f714d44dccc6c6dbca9f34f961fe8a262
jyamasak <jyamasak@sfu.ca>
2020-04-24 12:24:50 -0700

    Add map details to epea* test

--- epea_test.py
+++ epea_test.py
@@ -1,11 +1,14 @@
 import epea
 import map_utils
-import math
+from map_utils import MapDetails
+
+output_file_name = 'results/epea_test_output'
 
 def test_epea_with_valid_solution():
     file_name = "instances/exp2_1.txt"
     my_map, starts, goals = map_utils.import_mapf_instance(file_name)
-    epea_sol = epea.EPEASolver(my_map, starts, goals)
+    map_details = MapDetails(output_file_name, file_name, my_map, starts, goals)
+    epea_sol = epea.EPEASolver(map_details)
     solution = epea_sol.find_solution()
     assert solution == [[(1,1), (1,2), (1,3), (1,4), (1,5)],
                         [(1,2), (1,3), (2,3), (1,3), (1,4)]], "EPEA* could not find solution even though valid solution exists"
@@ -14,7 +17,8 @@ def test_epea_with_valid_solution():
 def test_epea_with_valid_solution2():
     file_name = "instances/exp2_2.txt"
     my_map, starts, goals = map_utils.import_mapf_instance(file_name)
-    epea_sol = epea.EPEASolver(my_map, starts, goals)
+    map_details = MapDetails(output_file_name, file_name, my_map, starts, goals)
+    epea_sol = epea.EPEASolver(map_details)
     solution = epea_sol.find_solution()
     assert solution == [[(1,2), (1,3), (2,3), (1,3), (1,4)],
                         [(1,1), (1,2), (1,3), (1,4), (1,5)]], "EPEA* could not find solution even though valid solution exists"
