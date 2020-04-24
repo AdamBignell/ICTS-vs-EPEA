b69b5f4c691e2ed067f55a36cfecaee3deb54cab
AdamBignell <adambignell@gmail.com>
2020-04-24 12:33:50 -0700

    EPEA Testing: Fix EPEA* testing to use new heuristic

--- epea_test.py
+++ epea_test.py
@@ -5,7 +5,8 @@ import math
 def test_epea_with_valid_solution():
     file_name = "instances/exp2_1.txt"
     my_map, starts, goals = map_utils.import_mapf_instance(file_name)
-    epea_sol = epea.EPEASolver(my_map, starts, goals)
+    map_details = map_utils.MapDetails(file_name, file_name, my_map, starts, goals)
+    epea_sol = epea.EPEASolver(map_details)
     solution = epea_sol.find_solution()
     assert solution == [[(1,1), (1,2), (1,3), (1,4), (1,5)],
                         [(1,2), (1,3), (2,3), (1,3), (1,4)]], "EPEA* could not find solution even though valid solution exists"
@@ -14,9 +15,10 @@ def test_epea_with_valid_solution():
 def test_epea_with_valid_solution2():
     file_name = "instances/exp2_2.txt"
     my_map, starts, goals = map_utils.import_mapf_instance(file_name)
-    epea_sol = epea.EPEASolver(my_map, starts, goals)
+    map_details = map_utils.MapDetails(file_name, file_name, my_map, starts, goals)
+    epea_sol = epea.EPEASolver(map_details)
     solution = epea_sol.find_solution()
-    assert solution == [[(1,2), (1,3), (2,3), (1,3), (1,4)],
+    assert solution == [[(1,2), (1,3), (1,4), (2,4), (1,4)],
                         [(1,1), (1,2), (1,3), (1,4), (1,5)]], "EPEA* could not find solution even though valid solution exists"
 
 if __name__ == '__main__':
